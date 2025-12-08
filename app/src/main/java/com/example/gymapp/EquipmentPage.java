3e611bac4a55248c400492d05448c811043647da
Rafly Adha Prathama <raflyadha11@gmail.com>
2025-12-08 16:34:46 +0700

    mengubah textView menjadi checkBox
    mengubah textView pada EquipmentPage, dan MuscleSelectionPage menjadi checkBox, dan sudah berfungsi, tetapi belum mengambil data check box nya

--- app/src/main/java/com/example/gymapp/EquipmentPage.java
+++ app/src/main/java/com/example/gymapp/EquipmentPage.java
@@ -4,15 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.Toast; // Import Toast
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EquipmentPage extends AppCompatActivity {
 
+    LinearLayout AdvMlayout, BscMachineLayout, DumbellLayout, BarbellLayout, BenchLayout, BodyWeightLayout;
+    CheckBox cb_advanceMachine, cb_basicMachine, cb_dumbell, cb_barbell, cb_bench, cb_bodyweight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipment_page);
+        AdvMlayout = findViewById(R.id.advMLayout);
+        BscMachineLayout = findViewById(R.id.bscMachineLayout);
+        DumbellLayout = findViewById(R.id.dumbbellLayout);
+        BarbellLayout = findViewById(R.id.barbellLayout);
+        BenchLayout = findViewById(R.id.benchLayout);
+        BodyWeightLayout = findViewById(R.id.bodyweightLayout);
+
+        cb_advanceMachine = findViewById(R.id.cbAdvancedMachine);
+        cb_basicMachine = findViewById(R.id.cbBasicMachine);
+        cb_dumbell = findViewById(R.id.cbDumbell);
+        cb_barbell = findViewById(R.id.cbBarbell);
+        cb_bench = findViewById(R.id.cbBench);
+        cb_bodyweight = findViewById(R.id.cbBodyWeight);
 
         Button btnBack1 = (Button) findViewById(R.id.btnBack1);
         btnBack1.setOnClickListener(new View.OnClickListener() {
@@ -32,8 +50,54 @@ public class EquipmentPage extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        DumbellLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_dumbell.setChecked(!cb_dumbell.isChecked());
+            }
+        });
+
+        AdvMlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_advanceMachine.setChecked(!cb_advanceMachine.isChecked());
+            }
+        });
+
+        BscMachineLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_basicMachine.setChecked(!cb_basicMachine.isChecked());
+            }
+        });
+
+        BarbellLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_barbell.setChecked(!cb_barbell.isChecked());
+            }
+        });
+
+        BenchLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_bench.setChecked(!cb_bench.isChecked());
+            }
+        });
+
+        BodyWeightLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_bodyweight.setChecked(!cb_bodyweight.isChecked());
+            }
+        });
     }
 
-    public void Dumbell(View view) {
-    }
 }
--- app/src/main/java/com/example/gymapp/MuscleSelectionPage.java
+++ app/src/main/java/com/example/gymapp/MuscleSelectionPage.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +14,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MuscleSelectionPage extends AppCompatActivity {
+    LinearLayout Shoulderlayout, ArmLayout, ChestLayout, BackLayout, LegLayout, BodyWeightLayout;
+    CheckBox cb_shoulder, cb_arm, cb_chest, cb_back, cb_leg, cb_bodyweight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_muscle_selection_page);
+
+        Shoulderlayout = findViewById(R.id.shoulderLayout);
+        ArmLayout = findViewById(R.id.armLayout);
+        ChestLayout = findViewById(R.id.chestLayout);
+        BackLayout = findViewById(R.id.backLayout);
+        LegLayout = findViewById(R.id.legLayout);
+        BodyWeightLayout = findViewById(R.id.bodyweightLayout);
+
+        cb_shoulder = findViewById(R.id.cbShoulder);
+        cb_arm = findViewById(R.id.cbArm);
+        cb_chest = findViewById(R.id.cbChest);
+        cb_back = findViewById(R.id.cbBack);
+        cb_leg = findViewById(R.id.cbleg);
+        cb_bodyweight = findViewById(R.id.cbBodyWeight);
 
         Button btnBack = (Button) findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +52,54 @@ public class MuscleSelectionPage extends AppCompatActivity {
 
                 Intent i = new Intent(MuscleSelectionPage.this, exercisesPage.class);
                 startActivity(i);
+            }
+        });
+
+        Shoulderlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_shoulder.setChecked(!cb_shoulder.isChecked());
+            }
+        });
+
+        ArmLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_arm.setChecked(!cb_arm.isChecked());
+            }
+        });
+
+        ChestLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_chest.setChecked(!cb_chest.isChecked());
+            }
+        });
+
+        BackLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_back.setChecked(!cb_back.isChecked());
+            }
+        });
+
+        LegLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_leg.setChecked(!cb_leg.isChecked());
+            }
+        });
+
+        BodyWeightLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Balikkan status checkbox (Checked -> Unchecked atau sebaliknya)
+                cb_bodyweight.setChecked(!cb_bodyweight.isChecked());
             }
         });
     }
